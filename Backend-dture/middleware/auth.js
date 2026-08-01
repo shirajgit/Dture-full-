7dddf919009180196bddb5bfe8bc981d74290d40
Shiraj Mujawar <shirajmujawar03@gmail.com>
2026-08-01 15:24:58 +0530

    Update error message for missing token

--- Backend-dture/middleware/auth.js
+++ Backend-dture/middleware/auth.js
@@ -4,7 +4,7 @@ const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    return res.status(401).json({ message: "No token provided as long" });
   }
 
   try {
